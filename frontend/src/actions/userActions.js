51e82630ffe52f733fb23a13c1cd3535f2c0cf96
Roman Jasiek <romanjasiek@me.com>
2022-01-09 22:17:11 +0100

    Login, logout implemented

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -35,3 +35,9 @@ export const login = (email, password) => async (dispatch) => {
     });
   }
 };
+
+export const logout = () => (dispatch) => {
+  dispatch({ type: USER_LOGOUT });
+
+  localStorage.removeItem('userInfo');
+}
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { LinkContainer } from 'react-router-bootstrap';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+import { logout } from '../actions/userActions';
 
 const Header = () => {
+  const dispatch = useDispatch();
+
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin;
+
+  const logoutHandler = () => {
+    dispatch(logout());
+  }
+
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
@@ -20,11 +31,28 @@ const Header = () => {
                   <i className='fas fa-shopping-cart categoryColor'></i> Cart
                 </Nav.Link>
               </LinkContainer>
-              <LinkContainer to='/login'>
-                <Nav.Link>
-                  <i className='fab fa-empire categoryColor'></i> Sign In
-                </Nav.Link>
-              </LinkContainer>
+              {userInfo ? (
+                <NavDropdown title={`Welcome, ${userInfo.name}`} id='username'>
+                  <LinkContainer to='/user'>
+                    <NavDropdown.Item>
+                      <i className='fas fa-user-circle categoryColor'></i>{' '}
+                      Profile
+                    </NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Divider />
+                  {/* <LinkContainer to='/logout'> */}
+                    <NavDropdown.Item onClick={logoutHandler}>
+                      <i className='fas fa-sign-out-alt categoryColor'></i>{' '}
+                      Logout
+                    </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to='/login'>
+                  <Nav.Link>
+                    <i className='fab fa-empire categoryColor'></i> Sign In
+                  </Nav.Link>
+                </LinkContainer>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
