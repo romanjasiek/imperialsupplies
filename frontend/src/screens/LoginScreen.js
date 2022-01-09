a3669f37fb37480b672da39460fdedeea9ce9ad6
Roman Jasiek <romanjasiek@me.com>
2022-01-09 21:49:45 +0100

    Login screen implemented

---
+++ frontend/src/screens/LoginScreen.js
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Form, Button, Row, Col } from 'react-bootstrap';
+import FormContainer from '../components/FormContainer';
+import { useDispatch, useSelector } from 'react-redux';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import { login } from '../actions/userActions';
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { search } = useLocation();
+  const history = useNavigate();
+
+  const dispatch = useDispatch();
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { loading, error, userInfo } = userLogin;
+
+  //   const redirect = location.search.split('=')[1];
+  const redirect = search ? search.split('=')[1] : '/';
+
+    useEffect(() => {
+        if (userInfo) {
+            history(redirect)
+        }
+    }, [history, userInfo, redirect]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    dispatch(login(email, password));
+  };
+
+  return (
+    <FormContainer>
+      <h1>Sign In</h1>
+      {error && <Message variant='danger'>{error}</Message>}
+      {loading && <Loader />}
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId='email'>
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Enter email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group controlId='password'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Enter Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className='d-grid gap-2'>
+          {' '}
+          <Button
+            variant='primary'
+            type='submit'
+            className='btn btn-danger btn-block my-2'
+          >
+            Sign In
+          </Button>
+        </Form.Group>
+      </Form>
+      <Row className='py-3'>
+        <Col className='d-flex justify-content-between'>
+          New Customer?{' '}
+          <Link
+            to={redirect ? `/register/redirect=${redirect}` : '/register'}
+            className='px-2 link-danger'
+          >
+            <i className='fab fa-empire categoryColor px-1'></i>Sign up here
+          </Link>
+          <Link className='ms-auto link-secondary' to='/'>
+            <i className='fas fa-arrow-alt-circle-left categoryColor px-1'></i>Go
+            back
+          </Link>
+        </Col>
+      </Row>
+    </FormContainer>
+  );
+};
+
+export default LoginScreen;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,18 +2,25 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { productListReducer, productDetailsReducer } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
+import { userLoginReducer } from './reducers/userReducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    userLogin: userLoginReducer
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [];
 
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null;
+
 const initialState = {
-  cart: { cartItems: cartItemsFromStorage }
+  cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage }
 };
 
 const middleware = [thunk];
