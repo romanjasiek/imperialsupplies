e92f7324f209c87124b80ea34c47bc064a13a572
Roman Jasiek <romanjasiek@me.com>
2022-01-10 14:53:48 +0100

    Started working on checkout process

---
+++ frontend/src/screens/ShippingScreen.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Form, Button, Row, Col } from 'react-bootstrap';
+import FormContainer from '../components/FormContainer';
+import { useDispatch, useSelector } from 'react-redux';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+
+const ShippingScreen = () => {
+  return <div>Shipping</div>;
+};
+
+export default ShippingScreen;
