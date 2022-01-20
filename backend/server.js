710ee61a3f953b127d4f089e9f2225e3ed81f4f9
Roman Jasiek <romanjasiek@me.com>
2022-01-20 10:52:05 +0100

    Fixing mixed content warning in Chrome

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,7 @@ cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECRET,
+  secure: true
 });
 app.use('/api/products', productRoutes);
 
