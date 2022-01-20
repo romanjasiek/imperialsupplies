154b714af4e3a23eddb6ab4aecf41fe654490d3e
Roman Jasiek <romanjasiek@me.com>
2022-01-20 11:04:34 +0100

    Cloudinary fixes

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,6 @@ cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECRET,
-  secure: true
 });
 app.use('/api/products', productRoutes);
 
